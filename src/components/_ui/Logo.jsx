aba53a8d7476df91e8dda5405c6d9829fae0a293
Christian Guevara <christianguevara@Christians-MacBook-Pro.local>
2025-02-10 17:37:39 -0500

    Initial commit 2.0

--- src/components/_ui/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import logo from '../../images/logo-firma-horizontal-the-dveloper.png';
-
-const Logo = (props) => (
-  <img {...props} src={logo} alt="The Dveloper logo" />
-);
-
-export default Logo;
