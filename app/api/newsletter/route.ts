db47e0cc6480a43864d81dd1cef5ba9d9360bf37
Christian Guevara <christianguevara@Christians-MacBook-Pro.local>
2025-02-11 22:38:12 -0500

    Fix newsletter api

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -1,11 +1,12 @@
-import { NewsletterAPI } from 'pliny/newsletter'
-import siteMetadata from '@/data/siteMetadata'
-
 export const dynamic = 'force-static'
 
-const handler = NewsletterAPI({
-  // @ts-ignore
-  provider: siteMetadata.newsletter.provider,
-})
+// import { NewsletterAPI } from 'pliny/newsletter'
+// import siteMetadata from '@/data/siteMetadata'
 
+// Original newsletter subscription handler code commented out
+// const handler = NewsletterAPI({
+//   provider: siteMetadata.newsletter.provider,
+// })
+
+const handler = () => new Response(null, { status: 200 })
 export { handler as GET, handler as POST }
