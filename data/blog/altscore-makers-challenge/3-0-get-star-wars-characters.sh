ed6a8817424ef63f3411eb8517dfa2575e2c3f14
Christian Guevara <christian@thedveloper.com>
2025-03-27 17:52:55 -0500

    Add some comments

--- data/blog/altscore-makers-challenge/3-0-get-star-wars-characters.sh
+++ data/blog/altscore-makers-challenge/3-0-get-star-wars-characters.sh
@@ -5,15 +5,17 @@ url="https://swapi.dev/api/people"
 output_file="star_wars_characters_with_homeworld.txt"
 > "$output_file"
 # Go page by page until apparently get null
-#TODO: test if null is right
 while [ -n "$url" ]; do
     response=$(curl -s "$url")
     
+    # Get the characters and their homeworld id
     echo "$response" | jq -r '.results[] | 
         (.name + ":" + (.homeworld | split("/")[-2]))' >> "$output_file"
     
-    url=$(echo "$response" | jq -r '.next')
+    # Get the next page
+    url=$(echo "$response" | jq -r '.next') 
     
+    # If the next page is null, break the loop
     if [ "$url" = "null" ]; then
         url=""
     fi
