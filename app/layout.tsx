4bb28b04f2ad63462df20c3e4ed47220dcc285c5
Christian Guevara <christian@thedveloper.com>
2025-03-27 12:23:28 -0500

    Add Vercel SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,14 @@ import 'remark-github-blockquote-alert/alert.css'
 
 import { Space_Grotesk } from 'next/font/google'
 // import { Analytics, AnalyticsConfig } from 'pliny/analytics'
-import { SearchProvider, SearchConfig } from 'pliny/search'
+import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import SectionContainer from '@/components/SectionContainer'
-import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
-import { ThemeProviders } from './theme-providers'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Metadata } from 'next'
+import { SearchConfig, SearchProvider } from 'pliny/search'
+import { ThemeProviders } from './theme-providers'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -93,6 +94,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
               <Header />
               <main className="mb-auto">{children}</main>
+              <SpeedInsights />
             </SearchProvider>
             <Footer />
           </SectionContainer>
