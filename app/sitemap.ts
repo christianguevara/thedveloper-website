7780f791523f642caeca9d3e0b6172bef194186a
Christian Guevara <christian@thedveloper.com>
2025-03-27 12:25:49 -0500

    use filterPosts for vercel sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,17 @@
 import { MetadataRoute } from 'next'
 import { allBlogs } from 'contentlayer/generated'
 import siteMetadata from '@/data/siteMetadata'
+import { filterPosts } from '@/utils/content'
 
 export const dynamic = 'force-static'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = siteMetadata.siteUrl
 
-  const blogRoutes = allBlogs
-    .filter((post) => !post.draft && post.listed)
-    .map((post) => ({
-      url: `${siteUrl}/${post.path}`,
-      lastModified: post.lastmod || post.date,
-    }))
+  const blogRoutes = filterPosts(allBlogs).map((post) => ({
+    url: `${siteUrl}/${post.path}`,
+    lastModified: post.lastmod || post.date,
+  }))
 
   const routes = ['', 'blog', 'projects', 'tags'].map((route) => ({
     url: `${siteUrl}/${route}`,
